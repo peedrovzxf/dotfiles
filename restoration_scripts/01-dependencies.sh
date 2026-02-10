de11c7ba3cf2c21c014895c3f676c82f41177b65
peedrovzxf <166252531+peedrovzxf@users.noreply.github.com>
2026-02-10 09:43:06 -0300

    feat(scripts): avoid duplicates on powersave

--- restoration_scripts/01-dependencies.sh
+++ restoration_scripts/01-dependencies.sh
@@ -10,7 +10,10 @@ read -n1 -rep 'Would you like to disable wifi powersave? (y,n)' WIFI
 if [[ $WIFI == "Y" || $WIFI == "y" ]]; then
     LOC="/etc/NetworkManager/conf.d/wifi-powersave.conf"
     echo -e "The following has been added to $LOC.\n"
-    echo -e "[connection]\nwifi.powersave = 2" | sudo tee -a $LOC
+    sudo tee "$LOC" >/dev/null <<'EOF'
+    [connection]
+    wifi.powersave = 2
+EOF
     echo -e "\n"
     echo -e "Restarting NetworkManager service...\n"
     sudo systemctl restart NetworkManager
